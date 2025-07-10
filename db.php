e35a0a179c7041bc1cae9504fc8ad5d7a12b700f
Minerva1337 <lukas.holzmann2002@gmail.com>
2025-07-10 11:20:07 +0200

    db namen geändert

---
+++ db.php
@@ -0,0 +1,11 @@
+<?php
+$host = 'localhost:3306';
+$user = 'lukas_uni';
+$password = 'Mkpi44ja!'; // ggf. anpassen
+$database = 'journalismus';
+
+$conn = new mysqli($host, $user, $password, $database);
+if ($conn->connect_error) {
+    die("Verbindung fehlgeschlagen: " . $conn->connect_error);
+}
+?>
