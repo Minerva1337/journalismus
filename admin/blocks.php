651081066bc68375e6830fffe7bf04a08685bc59
Minerva1337 <lukas.holzmann2002@gmail.com>
2025-07-10 11:40:00 +0200

    DB Struktur aktualliesert

--- admin/blocks.php
+++ admin/blocks.php
@@ -1,5 +1,7 @@
 <?php
+include 'auth.php';
 include 'db.php';
+
 $result = $conn->query("SELECT * FROM blocks");
 ?>
 <h2>Blöcke verwalten</h2>
@@ -7,19 +9,17 @@ $result = $conn->query("SELECT * FROM blocks");
 <table border="1" cellpadding="5">
   <tr>
     <th>Name</th>
-    <th>Kategorie</th>
-    <th>Basiswert</th>
-    <th>Faktor</th>
-    <th>Max Budget</th>
+    <th>Kosten</th>
+    <th>Reichweite</th>
+    <th>Qualität</th>
     <th>Aktionen</th>
   </tr>
   <?php while ($row = $result->fetch_assoc()): ?>
     <tr>
       <td><?= htmlspecialchars($row['name']) ?></td>
-      <td><?= htmlspecialchars($row['category']) ?></td>
-      <td><?= $row['base_value'] ?></td>
-      <td><?= $row['multiplier'] ?></td>
-      <td><?= $row['max_budget'] ?></td>
+      <td><?= $row['kosten'] ?></td>
+      <td><?= $row['reichweite'] ?></td>
+      <td><?= $row['qualität'] ?></td>
       <td>
         <a href="blocks_edit.php?id=<?= $row['id'] ?>">Bearbeiten</a> |
         <a href="blocks_delete.php?id=<?= $row['id'] ?>" onclick="return confirm('Wirklich löschen?')">Löschen</a>
